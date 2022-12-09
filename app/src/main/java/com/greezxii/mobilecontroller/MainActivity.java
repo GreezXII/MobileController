6d4dbebe9a74dec8df11e2aa39acdff5a43ac452
Кирилл Новиков <GreezXII@protonmail.ch>
2022-12-09 14:25:55 +0300

    Implement intent for settings activity.

--- app/src/main/java/com/greezxii/mobilecontroller/MainActivity.java
+++ app/src/main/java/com/greezxii/mobilecontroller/MainActivity.java
@@ -1,7 +1,11 @@
 package com.greezxii.mobilecontroller;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void initButtons()
+    {
+        // Settings button
+        Button button_settings = findViewById(R.id.button_settings);
+        button_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
