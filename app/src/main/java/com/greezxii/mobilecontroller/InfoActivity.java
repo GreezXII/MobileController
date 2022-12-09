36228b749f967750e2f4191b566cce452033f4e1
Кирилл Новиков <GreezXII@protonmail.ch>
2022-12-09 15:37:21 +0300

    Add movement method for info TextView.

--- app/src/main/java/com/greezxii/mobilecontroller/InfoActivity.java
+++ app/src/main/java/com/greezxii/mobilecontroller/InfoActivity.java
@@ -3,6 +3,8 @@ package com.greezxii.mobilecontroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        TextView info = findViewById(R.id.textView_info);
+        info.setMovementMethod(new ScrollingMovementMethod());
     }
 }
