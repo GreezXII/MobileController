51417509f5afce618a740990af95d580e550c95a
Кирилл Новиков <GreezXII@protonmail.ch>
2022-12-09 15:04:46 +0300

    FIX: add call for initButtons method.

--- app/src/main/java/com/greezxii/mobilecontroller/MainActivity.java
+++ app/src/main/java/com/greezxii/mobilecontroller/MainActivity.java
@@ -13,16 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButtons();
     }
 
     private void initButtons()
     {
+        Button button;
         // Settings button
-        Button button_settings = findViewById(R.id.button_settings);
-        button_settings.setOnClickListener(new View.OnClickListener() {
+        button = findViewById(R.id.button_settings);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+        // Open info button
+        button = findViewById(R.id.button_openInfo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
                 startActivity(intent);
             }
         });
