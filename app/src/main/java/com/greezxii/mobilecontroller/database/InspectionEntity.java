fb5901361534850926dac19db6a40b8ae7cbfb45
Кирилл Новиков <GreezXII@protonmail.ch>
2022-12-16 16:39:08 +0300

    Implement InspectionEntity model

---
+++ app/src/main/java/com/greezxii/mobilecontroller/database/InspectionEntity.java
@@ -0,0 +1,36 @@
+package com.greezxii.mobilecontroller.database;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+public class InspectionEntity {
+    @PrimaryKey
+    public int id;
+    public String street;
+    public int buildingNumber;
+    public char buildingLetter;
+    public int blockNumber;
+    public char blockLetter;
+    public int apartmentNumber;
+    public char apartmentLetter;
+    public String fullName;
+    public String meterSerialId;
+    public String meterModel;
+    public Date paymentDate;
+    public BigDecimal debt;
+    public Date lastInspectionDate;
+    public int value;
+    public boolean isAntimagnet;
+    public boolean isDisabled;
+    public BigDecimal debtByActs;
+    public String contacts;
+    public Date installationDate;
+    public Date vefiricationDate;
+    public int lastValue;
+    public String info;
+}
