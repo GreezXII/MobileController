a6d3ce71c79ce73e6c624c3aa84fe8b63bae0c4b
Kirill Novikov <GreezXII@protonmail.ch>
2022-12-28 13:11:22 +0300

    Replace Date type with LocalDate type in type converters

--- app/src/main/java/com/greezxii/mobilecontroller/database/Converters.java
+++ app/src/main/java/com/greezxii/mobilecontroller/database/Converters.java
@@ -1,19 +1,19 @@
 package com.greezxii.mobilecontroller.database;
 
 import androidx.room.TypeConverter;
-
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Converters {
     @TypeConverter
-    public static Date fromTimestamp(Long value) {
-        return value == null ? null : new Date(value);
+    public static LocalDate timestampToLcalDate(Long value) {
+        return value == null ? null : LocalDate.ofEpochDay(value);
     }
 
     @TypeConverter
-    public static Long dateToTimestamp(Date date) {
-        return date == null ? null : date.getTime();
+    public static Long localDateToTimestamp(LocalDate date) {
+        return date == null ? null : date.toEpochDay();
     }
 
     @TypeConverter
