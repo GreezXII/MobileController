f2f566a2cbd47101328c483afeca8cefa4769c20
Kirill Novikov <GreezXII@protonmail.ch>
2022-12-28 15:11:56 +0300

    Implement parsing of entities from file on TFTP server to List of Entity type

--- app/src/main/java/com/greezxii/mobilecontroller/database/MobileControllerDatabase.java
+++ app/src/main/java/com/greezxii/mobilecontroller/database/MobileControllerDatabase.java
@@ -5,7 +5,6 @@ import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
 @Database(version = 1, entities = {InspectionEntity.class})
-@TypeConverters(Converters.class)
 abstract class MobileControllerDatabase extends RoomDatabase {
     public abstract InspectionDao inspectionDao();
 }
