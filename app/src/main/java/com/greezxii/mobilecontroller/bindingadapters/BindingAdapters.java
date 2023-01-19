412e4342a61dcba52e7c68cff86df3f631aad73d
Kirill Novikov <GreezXII@protonmail.ch>
2023-01-19 14:01:35 +0300

    Implement binding adapter for meter

--- app/src/main/java/com/greezxii/mobilecontroller/bindingadapters/BindingAdapters.java
+++ app/src/main/java/com/greezxii/mobilecontroller/bindingadapters/BindingAdapters.java
@@ -2,6 +2,7 @@ package com.greezxii.mobilecontroller.bindingadapters;
 
 import android.widget.TextView;
 import androidx.databinding.BindingAdapter;
+import com.greezxii.mobilecontroller.database.Inspection;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -11,5 +12,14 @@ public class BindingAdapters {
     public static void setDate(TextView view, LocalDate date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy", new Locale("ru"));
         view.setText(formatter.format(date));
+    }
+    @BindingAdapter("meter")
+    public static void setMeter(TextView view, Inspection inspection) {
+        String result = String.format(new Locale("ru"),
+                "%s  № %s [%d]",
+                inspection.meterModel,
+                inspection.meterSerialId,
+                inspection.numberOfDigits);
+        view.setText(result);
     }
 }
